5dcc523c3c6d91dc90b2666a56a2b77511a862dd
rupeshagarwalg <142991136+rupeshagarwalg@users.noreply.github.com>
2025-10-29 18:19:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+const SQUARES = 800;
+
+for (let i = 0; i < SQUARES; i++) {
+	const square = document.createElement('div');
+	square.classList.add

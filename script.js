222eae63a952572b9d9f1362d0fad0f9790003ee
rupeshagarwalg <142991136+rupeshagarwalg@users.noreply.github.com>
2025-10-29 18:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-//your JS code here. If required.
 const container = document.querySelector('.container');
 const SQUARES = 800;
 
 for (let i = 0; i < SQUARES; i++) {
-	const square = document.createElement('div');
-	square.classList.add
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseout', () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = 'rgb(29, 29, 29)';
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
